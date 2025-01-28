ad186bb07046fa373734767cbb2d65908c4ef4ce
Ntawigenga <91368322+kel1y@users.noreply.github.com>
2025-01-28 17:14:33 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Wallet',
+  description: 'Personal Wallet',
 }
 
 export default function RootLayout({
